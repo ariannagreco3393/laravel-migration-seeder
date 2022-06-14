ba4c0e53ca9f67850e158a106e30bbe6eca22c2c
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-14 15:37:42 +0200

    add header footer and main create db sql link css install bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ mix.js('resources/js/app.js', 'public/js')
+ .copyDirectory('resources/img', 'public/img')
+ .options({
+     processCssUrls : false
+ })
+ .sass('resources/sass/app.scss', 'public/css');
